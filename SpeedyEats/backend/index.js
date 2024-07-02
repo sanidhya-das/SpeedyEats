a82105b24be7145ece3e0b57995639e2af5969c6
Sanidhya Das <sanidhyashubham2211@gmail.com>
2024-07-02 19:04:53 +0530

    Add files via upload

---
+++ SpeedyEats/backend/index.js
@@ -0,0 +1,31 @@
+
+global.foodData = require('./db')(function call(err, data, CatData) {
+  // console.log(data)
+  if(err) console.log(err);
+  global.foodData = data;
+  global.foodCategory = CatData;
+})
+
+const express = require('express')
+const app = express()
+const PORT = 5000
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://speedyeatss.netlify.app");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.use('/api/auth', require('./Routes/Auth'));
+
+app.listen(process.env.PORT || PORT, () => {
+  console.log(`Example app listening on http://localhost:${PORT}`)
+})
+
